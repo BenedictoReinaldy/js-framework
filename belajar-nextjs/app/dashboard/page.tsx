7b4677149c39ae0503418e6b2ca7a49b31aa6a04
BenedictoReinaldy <reinaldybenedict13@gmail.com>
2024-09-05 13:39:39 +0700

    add daisyui to nextjs

--- belajar-nextjs/app/dashboard/page.tsx
+++ belajar-nextjs/app/dashboard/page.tsx
@@ -4,6 +4,12 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Ini adalah halaman dashboard</h1>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
     </div>
   );
 }
